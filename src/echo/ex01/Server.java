9dde9cfed74d397a6f3e0e71ab2c66b2c2119a66
rlatjgus960 <rlatjgus960@naver.com>
2021-06-18 16:40:36 +0900

    6/18일 echo 프로그램 (스트림 사용)

--- src/echo/ex01/Server.java
+++ src/echo/ex01/Server.java
@@ -1,26 +1,71 @@
 package echo.ex01;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
-	public static void main(String[] args) throws IOException {
-		 ServerSocket serverSocket = new ServerSocket();
-		 serverSocket.bind(new InetSocketAddress("192.168.0.109",10001)); //IP, 포트번호 세팅, 일단 새로 생성해서 넣는거 외우기
-		 
-		 
-		 System.out.println("<서버시작>");
-		 System.out.println("================================================");
-		 System.out.println("[연결을 기다리고 있습니다.]");
-		 
-		 Socket socket = serverSocket.accept(); //new Socket~ 하는게 accept 안에 내장되어있음 소켓이라는 종이컵을 만들어줬다~
-		 System.out.println("[클라이언트가 연결되었습니다.");
+	// 필드
 
-		 socket.close();
-		 serverSocket.close();
+	// 생성자
+
+	// 메소드gs
+
+	// 메소드 일반
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket serverSocket = new ServerSocket();
+		serverSocket.bind(new InetSocketAddress("192.168.0.4", 10001)); // IP, 포트번호 세팅, 일단 새로 생성해서 넣는거 외우기
+
+		System.out.println("<서버시작>");
+		System.out.println("================================================");
+		System.out.println("[연결을 기다리고 있습니다.]");
+
+		Socket socket = serverSocket.accept(); // new Socket~ 하는게 accept 안에 내장되어있음 소켓이라는 종이컵을 만들어줬다~
+		System.out.println("[클라이언트가 연결되었습니다.");
+
+		// 메세지 받기용 스트림
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+
+		// 메세지 보내기용 스트림
+		OutputStream os = socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
+
+		while (true) {
+
+			// 메세지 받기
+			String msg = br.readLine();
+
+			if (msg == null) {
+				System.out.println("[클라이언트 접속 종료]");
+				break;
+			}
+			System.out.println("받은메세지:" + msg);
+
+			// 메세지 보내기
+			bw.write(msg);
+			bw.newLine();
+			bw.flush();
+		}
+
+		System.out.println("=======================================");
+		System.out.println("<서버종료>");
+
+		br.close();
+		bw.close();
+		socket.close();
+		serverSocket.close();
 	}
 
 }
